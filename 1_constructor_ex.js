11f2cc3fa564f4891e911e250995f024e6c43c13
Schiro Francesca <francesca.schiro@sisal.it>
2018-11-15 17:36:39 +0100

    added exercises with constructor and prototype

---
+++ 1_constructor_ex.js
@@ -0,0 +1,39 @@
+// Exercises with Constructor and Prototype
+
+function MyObj(a) {
+  this.prop1 = a;
+}
+MyObj.prototype.incr = function() {
+  return (this.prop1 = this.prop1 + 1);
+};
+MyObj.prototype.decr = function() {
+  return (this.prop1 = this.prop1 - 1);
+};
+
+var obj1 = new MyObj(1);
+var obj2 = new MyObj(20);
+var obj3 = new MyObj(234);
+var obj4 = new MyObj(44);
+
+obj1.incr = function() {
+  return (this.prop1 = this.prop1 + 5);
+};
+
+console.log(obj1.incr()); // 6
+console.log(obj2.incr()); // 21
+console.log(obj3.incr()); // 235
+console.log(obj4.incr()); // 45
+
+obj3.__proto__.incr = function() {
+  return (this.prop1 = "");
+};
+
+console.log(obj1.incr()); // 11
+console.log(obj2.incr()); // ""
+console.log(obj3.incr()); // ""
+console.log(obj4.incr()); // ""
+
+console.log(obj1.incr === obj2.__proto__.incr); // false
+console.log(obj1.incr === obj1.__proto__.incr); // false
+console.log(obj1.__proto__.incr === obj4.__proto__.incr); // true
+console.log(obj2.__proto__.incr === obj3.__proto__.incr); // true
--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 (dfasdf)
 
-## Learning JavaScript Design Patterns - A (book)[https://addyosmani.com/resources/essentialjsdesignpatterns/book/] by Addy Osmani 
+## Learning JavaScript Design Patterns 
+A (book)[https://addyosmani.com/resources/essentialjsdesignpatterns/book/] by Addy Osmani 
+---
 
 Practice of the **JavaScript Design Patterns**
-> run any file from the terminal with the command: `node *filename.js*`
+> * Run any file from your terminal with the command: `node *filename.js*`
 
 # 1. The Constructor Pattern
 *1_constructor.js* 
 
 # 2. The Module Pattern
+*2_modules.js*
